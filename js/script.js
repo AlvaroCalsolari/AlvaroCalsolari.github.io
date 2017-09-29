893712339f7fd5205c0ec9f3f7b1d321b9232c4a
AlvaroCalsolari <alvarocalsolari@hotmail.com>
2017-09-29 14:46:22 -0300

    v11

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 function printStars(stars){
 	// document.write("Hello World!");
+	var i = 0;
+	// console.log(i);
     for(var i = 0; i < stars; i++){
         document.write('<i class="fa fa-star" aria-hidden="true"></i>');
+	// console.log(i);
     }
+	// console.log("-->" + i);
+	for(var i = i ; i < 5 ; i ++ ){
+		document.write('<i class="fa fa-star-o" aria-hidden="true"></i>');
+	}
 }
